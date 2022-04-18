218d261f8f929033e905c6e7770484971878f19b
Ieva <Ieva@LVLANIE01>
2022-04-18 14:15:43 +0300

    Add project files.

---
+++ 2_Tema_1_uzdevums/Program.cs
@@ -0,0 +1,139 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+////1.uzdevums
+
+//Console.WriteLine("- Kā tevi sauc?");
+
+//string username = Console.ReadLine();
+
+//string userGreeting = "- Sveiks, " + username + "?";
+
+//Console. WriteLine(userGreeting);
+
+Console.WriteLine();
+Console.WriteLine("======================================================");
+Console.WriteLine();
+
+////2.uzdevums
+
+//Console.WriteLine("Kāds ir tavs vecums?");
+
+//int userNumber = int.Parse(Console.ReadLine());
+
+//int addedToUserNumber = userNumber + 1;
+
+//Console.WriteLine("Nākamgad tev paliks " + addedToUserNumber + "." + " Tu esi pilngadīgs!");
+
+Console.WriteLine();
+Console.WriteLine("======================================================");
+Console.WriteLine();
+
+////3.uzdevums
+
+//Console.WriteLine("Lūdzu, ievadi pirmo skaitli");
+//int firstNumber = int.Parse(Console.ReadLine());
+//Console.WriteLine("Lūdzu, ievadi otro skaitli");
+//int secondNumber = int.Parse(Console.ReadLine());
+//Console.WriteLine("Lūdzu, ievadi trešo skaitli");
+//int thirdNumber = int.Parse(Console.ReadLine());
+//Console.WriteLine("Lūdzu, ievadi ceturto skaitli");
+//int fourthNumber = int.Parse(Console.ReadLine());
+
+//int firstMaxNumber = Math.Max(firstNumber, secondNumber);
+//int secondMaxNumber = Math.Max(thirdNumber, fourthNumber);
+//int maxNumber = Math.Max(firstMaxNumber, secondMaxNumber);
+
+//Console.WriteLine(maxNumber);
+
+
+Console.WriteLine();
+Console.WriteLine("======================================================");
+Console.WriteLine();
+
+
+////4.uzdevums
+
+//Console.WriteLine("Lūdzu, ievadi pirmo skaitli");
+//int firstNumber = int.Parse(Console.ReadLine());
+//Console.WriteLine("Lūdzu, ievadi otro skaitli");
+//int secondNumber = int.Parse(Console.ReadLine());
+//Console.WriteLine("Lūdzu, ievadi trešo skaitli");
+//int thirdNumber = int.Parse(Console.ReadLine());
+//Console.WriteLine("Lūdzu, ievadi ceturto skaitli");
+//int fourthNumber = int.Parse(Console.ReadLine());
+
+//int firstMinNumber = Math.Min(firstNumber, secondNumber);
+//int secondMinNumber = Math.Min(thirdNumber, fourthNumber);
+//int minNumber = Math.Min(firstMinNumber, secondMinNumber);
+
+//Console.WriteLine(minNumber);
+
+
+Console.WriteLine();
+Console.WriteLine("======================================================");
+Console.WriteLine();
+
+//5.uzdevums
+
+//Console.WriteLine("Lūdzu, ievadi pirmo skaitli");
+//int firstNumber = int.Parse(Console.ReadLine());
+//Console.WriteLine("Lūdzu, ievadi otro skaitli");
+//int secondNumber = int.Parse(Console.ReadLine());
+
+//int result = firstNumber % secondNumber;
+//Console.WriteLine(result);
+
+
+Console.WriteLine();
+Console.WriteLine("======================================================");
+Console.WriteLine();
+
+////6.uzdevums
+
+//Console.WriteLine("Lūdzu, ievadi skaitli");
+//int number = int.Parse(Console.ReadLine());
+//bool isEven = number % 2 == 0;
+//Console.WriteLine("Skaitlis ir pāra skaitlis: " + isEven);
+
+
+Console.WriteLine();
+Console.WriteLine("======================================================");
+Console.WriteLine();
+
+////7.uzdevums
+
+//Console.WriteLine("Lūdzu ievadi pirmo skaitli");
+//int firstNumber = int.Parse(Console.ReadLine());
+//Console.WriteLine("Lūdzu ievadi otro skaitli");
+//int secondNumber = int.Parse(Console.ReadLine());
+
+//int area = firstNumber * secondNumber;
+//Console.WriteLine(area);
+
+
+Console.WriteLine();
+Console.WriteLine("======================================================");
+Console.WriteLine();
+
+////8.uzdevums
+
+//Console.WriteLine("Lūdzu, ievadi trijstūra malas garumu");
+//int number = int.Parse(Console.ReadLine());
+
+//int area = number * number / 2;
+//Console.WriteLine(area);
+
+
+Console.WriteLine();
+Console.WriteLine("======================================================");
+Console.WriteLine();
+
+//9.uzdevums
+
+Console.WriteLine("- Kāds ir tavs vārds?");
+string userName = Console.ReadLine();
+
+Console.WriteLine("- Kāds ir tavs vecums?");
+string userAge = Console.ReadLine();
+
+Console.WriteLine($"Sveiks {userName}, Tavs vecums ir {userAge}.");
